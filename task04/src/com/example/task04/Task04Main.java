5f4981eb27fcdb397b6225d1e56de76f4d803480
NikMarcos <eronikita@yandex.ru>
2019-12-29 15:29:35 +0500

    task04

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -2,6 +2,9 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
+        Line l = new Line(new Point(10, 23), new Point(2, 14));
+        String l1 = l.toString();
+        System.out.println(l1);
 
     }
 }
